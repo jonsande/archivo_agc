241aac55e2c0c62231de9ee44f9f0454dfea351e
Jon <jonandersande@gmail.com>
2023-10-18 02:45:55 +0200

    Pequeños ajustes a los atributos del modelo (e.e. la clase) HomeConfig

--- core/models.py
+++ core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class HomeConfig(models.Model):
-    titulo = models.TextField(max_length=48, null=False, blank=False, default='Archivo de Tertulias de ¿Agustín García Calvo?')
-    subtitulo = models.TextField(max_length=64, null=False, blank=False, default='Bienvenidos al Archivo de Tertulias de Agustín García Calvo')
+    titulo = models.CharField(max_length=48, null=False, blank=False, default='Archivo de Tertulias de ¿Agustín García Calvo?')
+    subtitulo = models.CharField(max_length=64, null=False, blank=False, default='Bienvenidos al Archivo de Tertulias de Agustín García Calvo')
     texto_portada = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Texto de bienvenida', default="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at tempor lectus. Etiam pharetra, sem a sagittis blandit, tortor odio euismod risus, eu tristique est nisi id sem. Pellentesque sodales est a diam aliquet commodo. Duis lobortis ante nec eros luctus, a lacinia dolor feugiat. Fusce sollicitudin, felis tempus placerat imperdiet, turpis lacus placerat justo, eu ullamcorper lacus metus ut felis. Aenean fringilla et tortor at rutrum. Etiam at est eget nunc accumsan sagittis in vel orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin quis hendrerit tortor.")
 
     class Meta:
